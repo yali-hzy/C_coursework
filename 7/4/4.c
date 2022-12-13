4eec5d09830966aeeaeaaadba5e25220fe1c7ed9
yali-hzy <48376189+yali-hzy@users.noreply.github.com>
2022-12-13 18:42:51 +0800

    revise7

--- 7/4/4.c
+++ 7/4/4.c
@@ -1,46 +1,57 @@
-#include<stdio.h>
-#include<string.h>
+/*
+高精度进制转换
+可以处理超过 int 型范围的数字
+可处理带有 一个'-' 和 多个前导0 的情况
+思路：除 10 取余倒读数
+*/
+#include <stdio.h>
+#include <string.h>
+
 int main()
 {
-	int len,a[110],*p,*q,n=0,ans[110],i,r,isNegative=0;
-	char s[110],strans[110]={'\0'};
-	scanf("%s",s);
+	int len, a[110], *p, *q, n = 0, ans[110], i, r;
+	char symbol = '\0';
+	char s[110], strans[110]={'\0'};
+	scanf("%s", s);
 	len = strlen(s);
-	if(s[0]=='-')
-		isNegative=1;
-	for(i=isNegative;i<len;i++)
-		a[i]=s[i]-48;
-	a[len]=-1;
-	p=a+isNegative;
+	if(s[0] == '-' || s[0] == '+')
+		symbol = s[0];
+	for(i = (symbol != '\0'); i < len; i++)
+		a[i] = s[i] - '0';
+	a[len] = -1;
+	p = a + (symbol != '\0');
 	while(!(*p))
 		p++;
-	if(*p==-1)
+	if(*p == -1)
 	{
 		puts("0");
 		return 0;
 	}
+    
 	while(*p)
 	{
-		r=0;
-		for(q=p;(*q)>=0;q++)
-			r=(r*8+(*q))%10;
-		ans[++n]=r;
-		while(*p<10 && *(p+1)>=0)
+		r = 0;
+		for(q = p; (*q) >= 0; q++)
+			r = (r * 8 + (*q)) % 10;
+		ans[++n] = r;
+		while(*p < 10 && *(p+1) >= 0)
 		{
-			*(p+1)+=(*p)*8;
+			*(p + 1) += (*p) * 8;
 			p++;
 		}
-		for(q=p;(*q)>=0;q++)
+		for(q = p; (*q) >= 0; q++)
 		{
-			if(*(q+1)>=0)
-				*(q+1)+=(*q)%10*8;
-			*q/=10;
+			if(*(q + 1) >= 0)
+				*(q + 1) += (*q) % 10 * 8;
+			*q /= 10;
 		}
 	}
-	if(isNegative)
-		strans[0]='-';
-	for(i=n;i>=1;i--)
-		strans[n-i+isNegative]=ans[i]+48;
-	printf("%s",strans);
+    
+	if(symbol)
+		strans[0] = symbol;
+	for(i = n; i >= 1; i--)
+		strans[n - i + (symbol != '\0')] = ans[i] + '0';
+	printf("%s", strans);
 	return 0;
 }
+
--- 7/4/7.c
+++ 7/4/7.c
@@ -1,54 +1,77 @@
-#include<stdio.h>
-#include<string.h>
-const int N=10;
-int cmp(char *a,char *b)
+/*
+可处理超过 int 型的正整数
+可处理含 一个符号 和 多个前导0 的数
+*/
+#include <stdio.h>
+#include <string.h>
+const int N = 10;
+int cmp(char *a, char *b)
 {
-	int alen=strlen(a),blen=strlen(b),astart,bstart,m;
-	for(astart=0;astart<alen-1 && a[astart]=='0';astart++);
-	for(bstart=0;bstart<blen-1 && b[bstart]=='0';bstart++);
-	m=(alen-astart)-(blen-bstart);
+	int alen = strlen(a), blen = strlen(b), astart = 0, bstart = 0, m, anegative =0, bnegative = 0;
+	if(a[astart] == '+')
+        astart++;
+    else
+        if(a[astart] == '-')
+        {
+            anegative = 1;
+            astart++;
+        }
+    if(b[astart] == '+')
+        bstart++;
+    else
+        if(b[bstart] == '-')
+        {
+            bnegative = 1;
+            bstart++;
+        }
+   	if(anegative != bnegative)
+        return anegative < bnegative;
+	for(;astart < alen - 1 && a[astart] == '0'; astart++);
+	for(;bstart < blen - 1 && b[bstart] == '0'; bstart++);
+	m = (alen - astart) - (blen - bstart);
 	if(m)
 	{
-		if(m>0)
-			return 1;
+		if(m > 0)
+			return (1^anegative);
 		else
-			return 0;
+			return (0^anegative);
 	}
-	a+=astart;
-	b+=bstart;
-	for(;*a && *b;a++,b++)
+	a += astart;
+	b += bstart;
+	for(; *a && *b; a++, b++)
 	{
-		if(*a>*b)
-			return 1;
-		if(*a<*b)
-			return 0;
+		if(*a > *b)
+			return (1^anegative);
+		if(*a < *b)
+			return (0^anegative);
 	}
 	return 0;
 }
 int main()
 {
-	int i,j,opt,ilen,mlen,len;
-	char s[N+10][110],*minstr,tmp;
-	for(i=1;i<=N;i++)
-		scanf("%s",s[i]);
-	scanf("%d",&opt);
-	for(i=1;i<N;i++)
+	int i, j, opt, ilen, mlen, len;
+	char s[N+10][110], *minstr, tmp;
+	for(i = 1; i <= N; i++)
+		scanf("%s", s[i]);
+	scanf("%d", &opt);
+	for(i = 1; i < N; i++)
 	{
-		minstr=s[i];
-		for(j=i+1;j<=N;j++)
-			if((!opt && cmp(minstr,s[j])) || (opt && strcmp(minstr,s[j])>0))
-				minstr=s[j];
-		ilen=strlen(s[i]);
-		mlen=strlen(minstr);
-		len=ilen>mlen?ilen:mlen;
-		for(j=0;j<=len;j++)
+		minstr = s[i];
+		for(j = i + 1; j <= N; j++)
+			if((!opt && cmp(minstr, s[j])) || (opt && strcmp(minstr, s[j])>0))
+				minstr = s[j];
+		ilen = strlen(s[i]);
+		mlen = strlen(minstr);
+		len = ilen > mlen ? ilen : mlen;
+		for(j = 0; j <= len; j++)
 		{
-			tmp=s[i][j];
-			s[i][j]=minstr[j];
-			minstr[j]=tmp;
+			tmp = s[i][j];
+			s[i][j] = minstr[j];
+			minstr[j] = tmp;
 		}
 	}
-	for(i=1;i<=10;i++)
-		printf("%s\n",s[i]);
+	for(i = 1; i <= 10; i++)
+		printf("%s\n", s[i]);
 	return 0;
 }
+
